199b182e1bed771a67d425a1114eeff0117183df
Gian Marco Gherardi <gianmarco.gherardi@gmail.com>
2013-05-04 16:05:03 +0200

    work in progress server.

--- server/server.js
+++ server/server.js
@@ -3,19 +3,22 @@
 var express = require('express');
 var app = express();
 
-app.use('/api', express.bodyParser());
-app.use('/api', app.router);
-app.use('/', express.static(__dirname + '/..'));
+app.use(express.bodyParser());
+app.use(app.router);
+app.use(express.static(__dirname + '/..'));
 
 var items = JSON.parse(require('fs').readFileSync(__dirname + '/../items.json', { encoding: 'utf8' }));
+var findItem = function (id) {
+	return items.filter(function (item) { return item.id === id; })[0];
+};
 
-app.get('/items', function (req, res) {
+app.get('/api/items', function (req, res) {
 	res.json(items);
 });
 
-app.get('/items/:id', function (req, res) {
+app.get('/api/items/:id', function (req, res) {
 	var id = req.param('id'),
-		item = items.filter(function (item) { return item.id === id; })[0];
+		item = findItem(id);
 
 	if (item) {
 		res.json(item);
@@ -24,7 +27,45 @@ app.get('/items/:id', function (req, res) {
 	}
 });
 
-app.post('/items', function (req, res) {
+app.post('/api/items', function (req, res) {
+	var item = req.body;
+
+	item.id = Math.random().toString(36).substring(2);
+	item.comments = [];
+
+	items.push(item);
+
+	res.location('/api/items/' + item.id);
+	res.send(201);
+});
+
+app.put('/api/items/:id', function (req, res) {
+	var id = req.param('id'),
+		newItem = req.body,
+		item = findItem(id);
+
+	if (item) {
+		item.title = newItem.title;
+		item.tags = newItem.tags;
+		item.body = newItem.body;
+		res.send(200);
+	} else {
+		res.send(404);
+	}
+});
+
+app.get('/api/items/:id/comments', function (req, res) {
+	var id = req.param('id'),
+		item = findItem(id);
+
+	if (item) {
+		res.json(item.comments);
+	} else {
+		res.send(404);
+	}
+});
+
+app.post('/api/items/:id/comments', function (req, res) {
 	var newItem = req.body,
 		index = null;
 
@@ -37,7 +78,8 @@ app.post('/items', function (req, res) {
 	if (!index) {
 		index = items.length;
 		items.push({
-			id: Math.random().toString(36).substring(2)
+			id: Math.random().toString(36).substring(2),
+			comments: []
 		});
 	}
 
