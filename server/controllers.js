01858754c409f0920666b8444d57a4e3fa964e47
Gian Marco Gherardi <gianmarco.gherardi@gmail.com>
2013-05-11 16:09:23 +0200

    controllers moved to specific file.

---
+++ server/controllers.js
@@ -0,0 +1,75 @@
+"use strict";
+
+var items = JSON.parse(require('fs').readFileSync(__dirname + '/../items.json', { encoding: 'utf8' }));
+var findItem = function (id) {
+	return items.filter(function (item) { return item.id === id; })[0];
+};
+
+exports.getItems = function (req, res) {
+	res.json(items);
+};
+
+exports.getItem = function (req, res) {
+	var id = req.param('id'),
+		item = findItem(id);
+
+	if (item) {
+		res.json(item);
+	} else {
+		res.send(404);
+	}
+};
+
+exports.postItem = function (req, res) {
+	var item = req.body;
+
+	item.id = Math.random().toString(36).substring(2);
+	item.comments = [];
+
+	items.push(item);
+
+	res.location('/api/items/' + item.id);
+	res.send(201);
+};
+
+exports.putItem = function (req, res) {
+	var id = req.param('id'),
+		newItem = req.body,
+		item = findItem(id);
+
+	if (item) {
+		item.title = newItem.title;
+		item.tags = newItem.tags;
+		item.body = newItem.body;
+		res.send(200);
+	} else {
+		res.send(404);
+	}
+};
+
+exports.getComments = function (req, res) {
+	var id = req.param('id'),
+		newComment = req.body,
+		item = findItem(id);
+
+	if (item) {
+		res.json(item.comments);
+	} else {
+		res.send(404);
+	}
+};
+
+exports.postComment = function (req, res) {
+	var id = req.param('id'),
+		item = findItem(id);
+
+	if (item) {
+		item.comments.push({
+			itemId: id,
+			timestamp: new Date(),
+			text: newComment.text
+		});
+	} else {
+		res.send(404);
+	}
+};
--- server/server.js
+++ server/server.js
@@ -1,87 +1,20 @@
 "use strict";
 
-var express = require('express');
-var mongoose = require('mongoose');
+var express = require('express'),
+	mongoose = require('mongoose'),
+	ctrls = require('./controllers');
 var app = module.exports = express();
 
 app.use(express.bodyParser());
 app.use(app.router);
 app.use(express.static(__dirname + '/..'));
 
-var items = JSON.parse(require('fs').readFileSync(__dirname + '/../items.json', { encoding: 'utf8' }));
-var findItem = function (id) {
-	return items.filter(function (item) { return item.id === id; })[0];
-};
-
-app.get('/api/items', function (req, res) {
-	res.json(items);
-});
-
-app.get('/api/items/:id', function (req, res) {
-	var id = req.param('id'),
-		item = findItem(id);
-
-	if (item) {
-		res.json(item);
-	} else {
-		res.send(404);
-	}
-});
-
-app.post('/api/items', function (req, res) {
-	var item = req.body;
-
-	item.id = Math.random().toString(36).substring(2);
-	item.comments = [];
-
-	items.push(item);
-
-	res.location('/api/items/' + item.id);
-	res.send(201);
-});
-
-app.put('/api/items/:id', function (req, res) {
-	var id = req.param('id'),
-		newItem = req.body,
-		item = findItem(id);
-
-	if (item) {
-		item.title = newItem.title;
-		item.tags = newItem.tags;
-		item.body = newItem.body;
-		res.send(200);
-	} else {
-		res.send(404);
-	}
-});
-
-app.get('/api/items/:id/comments', function (req, res) {
-	var id = req.param('id'),
-		newComment = req.body,
-		item = findItem(id);
-
-	if (item) {
-		res.json(item.comments);
-	} else {
-		res.send(404);
-	}
-});
-
-app.post('/api/items/:id/comments', function (req, res) {
-	var id = req.param('id'),
-		item = findItem(id);
-
-	if (item) {
-		item.comments.push({
-			itemId: id,
-			timestamp: new Date(),
-			text: newComment.text
-		});
-	} else {
-		res.send(404);
-	}
-});
-
+app.get('/api/items', ctrls.getItems);
+app.get('/api/items/:id', ctrls.getItem);
+app.post('/api/items', ctrls.postItem);
+app.put('/api/items/:id', ctrls.putItem);
+app.get('/api/items/:id/comments', ctrls.getComments);
+app.post('/api/items/:id/comments', ctrls.postComment);
 
 if (!module.parent) {
 	mongoose.connect('mongodb://localhost/trackr');
